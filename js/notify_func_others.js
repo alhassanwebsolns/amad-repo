4d1e6a8643b701ad3d7a8041cdb9f549bfe0ebfd
TFC TECHNOLOGIES <114300815+alhassanwebsolns@users.noreply.github.com>
2023-05-22 06:07:42 +0400

    Update-015
    Update-015

---
+++ js/notify_func_others.js
@@ -0,0 +1,26 @@
+ $(function() {
+     setTimeout(function() {
+         $.notify({
+             // options
+             icon: '',
+             title: "<a href=\"../subs/fair-usage-disclaimer.html\"><h4>READ FAIR USAGE DISCLAIMER</h4>",
+             message: "<figure><img src=\"../assets/images/others/notify.png\"></figure><p>We take great care to ensure that all images used on our website...</a> "
+         }, {
+             // settings
+             icon_type: 'image',
+             type: 'info',
+             delay: 500,
+             timer: 3000,
+             z_index: 9999,
+             showProgressbar: false,
+             placement: {
+                 from: "bottom",
+                 align: "right"
+             },
+             animate: {
+                 enter: 'animated bounceInUp',
+                 exit: 'animated bounceOutDown'
+             },
+         });
+     }, 5000); // change the start delay
+ });
